213eddbcffb115d3ceca3c5ab7f554af622d95d3
desiaf <63371305+desiaf@users.noreply.github.com>
2020-04-09 01:26:38 +0530

    Add files via upload

---
+++ OpenUrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    public string Url;
+    public string Url2;
+    public void Open()
+    {
+        Application.OpenURL(Url);
+    }
+    public void CallPhone()
+    {
+        Application.OpenURL("tel://[8448603206]");
+    }
+
+    public void MarkAsDelivered()
+    {
+        Application.OpenURL(Url2);
+    }
+}
